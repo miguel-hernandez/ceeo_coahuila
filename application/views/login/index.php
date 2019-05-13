977abe40cbb0d683e6b078b681cdd51d81b658d5
Miguel Hernández <miguellhdez@gmail.com>
2019-05-13 09:24:51 -0500

    Logotipo Coahuila

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -9,16 +9,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> CEEO COAHUILA </title>
 
-  <link rel="shortcut icon" href="<?php echo base_url('assets/img/favicon.png'); ?>">
-  <link href="<?php echo base_url('assets/bootstrap337/css/bootstrap.min.css'); ?>" rel="stylesheet" media="screen">
-  <link href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet" media="screen">
-  <link href="<?php echo base_url('assets/css/estilos-master.css'); ?>" rel="stylesheet" media="screen">
+  <link rel="shortcut icon" href="<?= base_url('assets/img/favicon.png') ?>">
+  <link href="<?= base_url('assets/bootstrap337/css/bootstrap.min.css') ?>" rel="stylesheet" media="screen">
+  <link href="<?= base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet" media="screen">
+  <link href="<?= base_url('assets/css/estilos-master.css') ?>" rel="stylesheet" media="screen">
 
-  <script src="<?php echo base_url('assets/jquery-3.2.1.min.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/jquery.validate.js'); ?>"></script>
-  <script src="<?php echo base_url('assets/bootstrap337/js/bootstrap.min.js'); ?>"></script>
+  <script src="<?= base_url('assets/jquery-3.2.1.min.js') ?>"></script>
+  <script src="<?= base_url('assets/jquery.validate.js') ?>"></script>
+  <script src="<?= base_url('assets/bootstrap337/js/bootstrap.min.js') ?>"></script>
 
-  <script type="text/javascript" src="<?php echo base_url("assets/js/login/login.js"); ?>"></script>
+  <script type="text/javascript" src="<?= base_url("assets/js/login/login.js") ?>"></script>
 
   <style>
   .div_white{
@@ -49,38 +49,40 @@
           <?php echo form_open('Login/validar_login', array("id"=>"form_login")); ?>
           <div class="row">
             <div class="col-xs-12">
-              <?= $this->session->flashdata(MESSAGEREQUEST); ?>
-              <img src="<?php echo base_url('assets/img/logosep.png'); ?>" alt="" class="img-responsive">
-            </div>
-          </div><!-- row -->
+              <center>
+                <?= $this->session->flashdata(MESSAGEREQUEST); ?>
+                <img src="<?php echo base_url('assets/img/logotipo1.png'); ?>" alt="" class="img-responsive">
+              </center>
+            </div><!-- .col-xs-12 -->
+          </div><!-- .row -->
 
           <div class="row margintop10">
             <div class="col-xs-12">
               <i class="fa fa-user-o" aria-hidden="true"></i>
-              <input id="" name="username" type="text" class="form-control" placeholder="usuario" autofocus>
-            </div>
-          </div><!-- row -->
+              <input name="username" type="text" class="form-control" placeholder="usuario" autofocus>
+            </div><!-- .col-xs-12 -->
+          </div><!-- .row -->
 
           <div class="row margintop10">
             <div class="col-xs-12">
               <i class="fa fa-key" aria-hidden="true"></i>
-              <input id="" name="clave" type="password" class="form-control" placeholder="contraseña">
-            </div>
-          </div><!-- row -->
+              <input name="clave" type="password" class="form-control" placeholder="contraseña">
+            </div><!-- .col-xs-12 -->
+          </div><!-- .row -->
 
           <div class="row margintop10">
             <div class="col-xs-12">
               <button type="submit" class="btn btn-primary btn-block">Iniciar sesión</button>
-            </div>
-          </div><!-- row -->
+            </div><!-- .col-xs-12 -->
+          </div><!-- .row -->
           <?php echo form_close(); ?>
         </div>
       </div>
 
       <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>
-    </div><!-- row -->
+    </div><!-- .row -->
 
-  </div><!-- container -->
+  </div><!-- .container -->
 
 </body>
 </html>
