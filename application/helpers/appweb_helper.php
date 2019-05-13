2f6937c2d7499678db25832019cccc4015ea5996
Miguel Hernández <miguellhdez@gmail.com>
2019-05-13 09:47:57 -0500

    Helpers PHP y uso de envia_datos_json

--- application/helpers/appweb_helper.php
+++ application/helpers/appweb_helper.php
@@ -9,30 +9,10 @@
         }// envia_datos_json()
     }
 
-    if(!function_exists('cerrar_sesion')){
-        function cerrar_sesion($contexto) {
-          $contexto->session->unset_userdata(array(ELEMENTOS_MENU, 'nombre_usuario', 'tipousuario', 'nombre_usuario_f', DATOSUSUARIO));
-          $contexto->session->sess_destroy();
-          redirect('/');
-        }// cerrar_sesion()
-    }
-
     if(!function_exists('hay_sesion_abierta')){
         function hay_sesion_abierta($contexto) {
       		return $contexto->session->has_userdata(DATOSUSUARIO);
       	}// hay_sesion_abierta()
-    }
-
-    if(!function_exists('verifica_sesion_redirige_ajax')){
-        function verifica_sesion_redirige_ajax($contexto) {
-      		$flag = TRUE;
-      		if (!hay_sesion_abierta($contexto)) {
-            $response = array('respuesta' => 'REDIRECT');
-            envia_datos_json(200, $response, $this);
-      			$flag = FALSE;
-      		}
-      		return $flag;
-      	}// verifica_sesion_redirige_ajax()
     }
 
 ?>
